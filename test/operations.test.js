1d4b97ca2bfbd21eab53d086779be8b376b7bdb0
Triceratroll <adrianmf06@gmail.com>
2022-05-09 21:37:33 +0200

    Fix requiere

--- test/operations.test.js
+++ test/operations.test.js
@@ -1,9 +1,9 @@
 const operations = require('../src/operations');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(operations.sum(1, 2)).toBe(3);
 });
 
 test('getSmallest(1, 2) to equal 1', () => {
-    expect(getSmallest(1, 2)).toBe(2);
+    expect(operations.getSmallest(1, 2)).toBe(2);
 })
